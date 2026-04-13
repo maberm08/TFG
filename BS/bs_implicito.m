1ce1c9c648d26bada35b75b96e5663bac8965f22
maberm08 <maberm08@ucm.es>
2026-04-13 13:15:58 +0200

    Update bs_implicito.m

--- BS/bs_implicito.m
+++ BS/bs_implicito.m
@@ -2,11 +2,11 @@ function [F,S,t] = bs_implicito(a,b,T,N,M,r,q,sigma,Phi,g1,g2)
 % ENTRADAS:
 %   a,b    : extremos del dominio espacial
 %   T      : vencimiento
-%   N,M    : número de subintervalos espacial y temporal
-%   r,q    : tipo de interés y dividendo continuo (ctes)
+%   N,M    : número de subintervalos espaciales y temporales
+%   r,q    : tipo de interés y dividendo continuo (constantes)
 %   sigma  : volatilidad
 %   Phi    : payoff terminal, función handle
-%   g1,g2  : condiciones de contorno, función handle
+%   g1,g2  : condiciones de contorno, funciones handle
 %
 % SALIDAS:
 %   F      : matriz de aproximaciones, F(i+1,j+1) ~ f(S_i,t_j)
@@ -44,18 +44,39 @@ function [F,S,t] = bs_implicito(a,b,T,N,M,r,q,sigma,Phi,g1,g2)
         gamma(i) = (tau/2) * (sigma^2*Si^2/h^2 + (r-q)*Si/h);
     end
 
-    % Matriz tridiagonal B
-    B = diag(beta) + diag(-gamma(1:N-2),1) + diag(-alpha(2:N-1),-1);
+    % Diagonales de la matriz B
+    subdiag_B = -alpha(2:end);
+    diag_B    = beta;
+    superdiag_B = -gamma(1:end-1);
+
+    % Factorización LU de B
+    [subdiag_L, diag_U, superdiag_U] = lu_tridiag(subdiag_B, diag_B, superdiag_B);
 
     % Iteración hacia atrás en el tiempo
     for j = M:-1:1
-        rhs = F(2:N,j+1);
+        segundo_miembro = F(2:N,j+1);
 
-        % Término d^j
-        rhs(1)   = rhs(1)   + alpha(1)   * g1(t(j));
-        rhs(end) = rhs(end) + gamma(end) * g2(t(j));
+        % Término d^j de contorno
+        d_j = zeros(N-1,1);
+        d_j(1)   = alpha(1)   * g1(t(j));
+        d_j(end) = gamma(end) * g2(t(j));
 
-        % Resolución del sistema
-        F(2:N,j) = B \ rhs;
+        segundo_miembro = segundo_miembro + d_j;
+
+        % Sustitución hacia delante: L y = segundo_miembro
+        y = zeros(N-1,1);
+        y(1) = segundo_miembro(1);
+        for k = 2:N-1
+            y(k) = segundo_miembro(k) - subdiag_L(k-1)*y(k-1);
+        end
+
+        % Sustitución hacia atrás: U solucion_interior = y
+        solucion_interior = zeros(N-1,1);
+        solucion_interior(end) = y(end) / diag_U(end);
+        for k = N-2:-1:1
+            solucion_interior(k) = (y(k) - superdiag_U(k)*solucion_interior(k+1)) / diag_U(k);
+        end
+
+        F(2:N,j) = solucion_interior;
     end
 end
